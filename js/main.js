95878ed9a171e74d9c597951869044d159b19fbe
Oscar Lopez <cake.owner@gmail.com>
2012-11-27 00:24:08 -0600

    Falling words effect

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
+/** Falling Elements **/
+function letItRain(x){
+	var elem = $("<div class='falling-elem'>node</div>");
+	elem.css("left",x);
+	elem.appendTo(document.body);
+	setTimeout( function (){
+		elem.css("top",screen.height);
+	},0);
+	elem.fadeOut(2000);
+}
+
 
